faa64ea887658c69aabd624e020aa040241fa0c7
Matheus Bernardi <matheusantoniobernardi@gmail.com>
2026-05-25 18:50:05 -0300

    a

--- apps/api/src/tasks/tasks.controller.ts
+++ apps/api/src/tasks/tasks.controller.ts
@@ -13,7 +13,7 @@ import {
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-// fah
+
 
 @Controller('tasks')
 export class TasksController {
